6ded1485e2863d0eb4e132d33e736f028f7cb4d7
JackDilger <jack.dilger1995@gmail.com>
2023-01-09 20:50:31 +0000

    Update state based on previous state

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -7,17 +7,33 @@ class StatefulGreeting extends React.Component {
         this.state = {
             introduction: "Hello!",
             buttonText: "Exit",
+            count: 0
         };
     }
 
     handleClick() {
-        this.setState({
-            introduction: "Goodbye!",
-            buttonText: "Enter",
-        }, ()=>{
-        console.log('new state', this.state.introduction)
-        console.log('new state', this.state.buttonText);
-        });
+        this.setState((prevState, prevProps) => {
+            console.log("Previous state:", prevState);
+            console.log("Previous state:", prevProps);
+        return {
+            introduction: prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+            buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+        };
+
+      });
+        
+    }
+
+    increment() {
+        this.setState((prevState, prevProps) => {
+            console.log("Previous state:", prevState);
+            console.log("Previous state:", prevProps);
+        return {
+            count: prevState.count + 1,
+        };
+
+      });
+
     }
 
     render () {
@@ -25,6 +41,8 @@ class StatefulGreeting extends React.Component {
             <div>
               <h1>{this.state.introduction} {this.props.name}! {this.props.greeting} </h1>
               <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+              <button onClick={() => this.increment()}>Increment</button>
+              <p>You've clicked {this.state.count} times</p>
             </div>
         )
     }
